fe6f2e14bbfb5cd716ad79e6b3ac2eace0713dea
Manish jain <jmanish21396@gmail.com>
2025-01-28 23:06:48 +0530

    new file added

---
+++ Quiz_App/app.js
@@ -0,0 +1,138 @@
+const questions = [
+  {
+    question: "Which is largest animal in the world?",
+    answers: [
+      {
+        text: "Shark",
+        correct: false,
+      },
+      {
+        text: "Blue Whale",
+        correct: true,
+      },
+      {
+        text: "Elephant",
+        correct: false,
+      },
+      {
+        text: "Giraffe",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Which is largest animal in the world?",
+    answers: [
+      {
+        text: "Shark",
+        correct: false,
+      },
+      {
+        text: "Blue Whale",
+        correct: true,
+      },
+      {
+        text: "Elephant",
+        correct: false,
+      },
+      {
+        text: "Giraffe",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Which is largest animal in the world?",
+    answers: [
+      {
+        text: "Shark",
+        correct: false,
+      },
+      {
+        text: "Blue Whale",
+        correct: true,
+      },
+      {
+        text: "Elephant",
+        correct: false,
+      },
+      {
+        text: "Giraffe",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Which is largest animal in the world?",
+    answers: [
+      {
+        text: "Shark",
+        correct: false,
+      },
+      {
+        text: "Blue Whale",
+        correct: true,
+      },
+      {
+        text: "Elephant",
+        correct: false,
+      },
+      {
+        text: "Giraffe",
+        correct: false,
+      },
+    ],
+  },
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHtml = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHtml = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+}
+
+startQuiz();
